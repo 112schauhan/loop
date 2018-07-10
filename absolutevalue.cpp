a5ae985a589b54fddaea507540a8dc7420723d1a
112schauhan <41046078+112schauhan@users.noreply.github.com>
2018-07-10 13:08:04 +0530

    Update absolutevalue.cpp

--- absolutevalue.cpp
+++ absolutevalue.cpp
@@ -1,17 +1,19 @@
 #include<iostream>
 int main()
 {
-    int i,n,sum=0;
-    std::cout<<"enter the value of n";
+    int i,u,n,m,sum=0;
+    std::cout<<"enter the lower limit";
+    std::cin>>m;
+    std::cout<<"enter upper limit";
     std::cin>>n;
-    for(i=1;i<n;i++)
+    for(i=1;i<=n;i++)
     {
-    	if(n%i==0)
+	if(n%i==0)
     	{
     		sum=sum+i;
 		}
 	}
-	if(sum==n)
+	if(sum==u)
 	{
 	  std::cout<<sum;
 	  std::cout<<"\n number is perfect";
